927e418c2c816548813cc523f6a0d17d8bb708ae
Lair Martes <lairmartes@gmail.com>
2023-04-26 21:17:49 -0500

    Show API field erros when user provides invalid data

--- src/app/service/useAPI.js
+++ src/app/service/useAPI.js
@@ -6,12 +6,12 @@ const SERVICE_KEY_STUDENTS_LIST = 'SERVICE_STUDENTS_LIST';
 const api_port = 8080;
 const api_url = 'http://localhost';
 const api_access_data = [
-    { key: SERVICE_KEY_STUDENTS_ADD, path:'/api/students/', successMessage: 'Student Included Successfully'},
-    { key: SERVICE_KEY_STUDENTS_LIST, path:'/api/students/all'}
+    { key: SERVICE_KEY_STUDENTS_ADD, path: '/api/students/', successMessage: 'Student Included Successfully' },
+    { key: SERVICE_KEY_STUDENTS_LIST, path: '/api/students/all' }
 ]
 
-const useSubmitForm = ({serviceKey, form, setFormData}) => {
-    
+const useSubmitForm = ({ serviceKey, form, setFormData }) => {
+
     const handleSubmit = (event) => {
 
         if (form) {
@@ -22,43 +22,41 @@ const useSubmitForm = ({serviceKey, form, setFormData}) => {
             const uri = api_url + ":" + api_port + serviceData.path;
 
             const data = Array.from(form.elements).filter((input) => input.name)
-                            .reduce((obj, input) => Object.assign(obj, { [input.name]: input.value }),{});
+                .reduce((obj, input) => Object.assign(obj, { [input.name]: input.value }), {});
 
-            fetch(uri, {        
+            fetch(uri, {
                 method: "POST",
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8'
                 },
                 body: JSON.stringify(data),
             })
-            .then((response) => {
-                //response. .forEach(v => console.log("header ===> " + v));
-                console.log("Response values ===> " + response);
+                .then((response) => {
 
-                if (response.status === 400) {
+                    if (response.status === 400) {
 
-                    response.text()
+                        response.text()
                             .then((text) => {
-                                const alertMessage = text.split(",").map(t => t.split(":").pop()).concat('\n');
+                                const alertMessage = text.split(",").map(t => "-" + t.split(":").pop()).join('\n');
 
-                                alert("User Error : " + alertMessage)
+                                alert("User Error(s) : \n\n" + alertMessage);
                             });
 
-                } else if (response.status === 200) {
-                    
-                    alert(serviceData.successMessage);
+                    } else if (response.status === 200) {
 
-                    setFormData({});
-                } else {
-                    throw new Error(response.statusText);
-                }
-            }).catch((err) => {
-                alert("Error: " + err.toString);
-            });
+                        alert(serviceData.successMessage);
+
+                        setFormData({});
+                    } else {
+                        throw new Error(response.statusText);
+                    }
+                }).catch((err) => {
+                    alert("System Error: " + err.toString);
+                });
         }
     }
 
     return { handleSubmit };
 }
 
-export {SERVICE_KEY_STUDENTS_ADD, SERVICE_KEY_STUDENTS_LIST, useSubmitForm }
+export { SERVICE_KEY_STUDENTS_ADD, SERVICE_KEY_STUDENTS_LIST, useSubmitForm }
